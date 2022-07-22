706235ede2be02e60a7ae78d5e84a167a6407591
Klajan <48261494+Klajan@users.noreply.github.com>
2022-07-22 16:57:45 +0200

    Add threadseafe processing class for JorunalEvents

---
+++ journal/EventProcessor.cpp
@@ -0,0 +1,58 @@
+#include "EventProcessor.h"
+
+EventProcessor::EventProcessor()
+{
+	this->counter = KillCounter();
+}
+
+EventProcessor::EventProcessor(KillCounter& counter)
+{
+	this->counter = counter;
+}
+
+void EventProcessor::processEventeQueue(std::unique_ptr<std::queue<std::unique_ptr<Journal::JournalEvent>>> queue)
+{
+	std::list<ThargoidKill> kills;
+	std::shared_ptr<Journal::SupercruiseExit> lastDrop;
+	std::shared_ptr<Journal::USSDrop> lastUss;
+	unsigned int uss_before_drop = 0;
+	if (!queue) return;
+	std::unique_ptr<Journal::JournalEvent> eve;
+	while (!queue->empty())
+	{
+		eve = std::move(queue->front());
+		queue->pop();
+		switch (eve->getEventType())
+		{
+		case Journal::EventType::USSDrop:
+			lastUss = std::shared_ptr<Journal::USSDrop>(static_cast<Journal::USSDrop*>(eve.release()));
+			uss_before_drop = 0x2;
+			break;
+		case Journal::EventType::SupercruiseExit:
+			lastDrop = std::shared_ptr<Journal::SupercruiseExit>(static_cast<Journal::SupercruiseExit*>(eve.release()));
+			uss_before_drop >>= 1;
+			break;
+		case Journal::EventType::KillBond:
+		{
+			std::shared_ptr<Journal::KillBond> bond(static_cast<Journal::KillBond*>(eve.release()));
+			counter.addKill(bond->thargoid(), bond->reward());
+			if (uss_before_drop)
+			{
+				kills.push_back(ThargoidKill(bond, lastDrop, lastUss));
+			}
+			else
+			{
+				kills.push_back(ThargoidKill(bond, lastDrop));
+			}
+		}
+		break;
+		default:
+			break;
+		}
+	}
+	static std::mutex listMutex;
+	{
+		std::scoped_lock lock(listMutex);
+		counter.mergeList(kills);
+	}
+}
---
+++ journal/EventProcessor.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <future>
+#include <queue>
+
+#include "events/JournalEventLib.h"
+#include "../ThargoidKill.h"
+#include "../KillCounter.h"
+
+class EventProcessor
+{
+private:
+	KillCounter counter;
+public:
+	EventProcessor();
+	EventProcessor(KillCounter& counter);
+
+	KillCounter& getKillCounter() { return counter; }
+
+	void processEventeQueue(std::unique_ptr<std::queue<std::unique_ptr<Journal::JournalEvent>>>);
+};
+
