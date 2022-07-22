03b2df23549a5ab36c6bebde5ea96b3e2ad8de9f
Klajan <48261494+Klajan@users.noreply.github.com>
2022-07-22 16:56:38 +0200

    Update KillCounter to store ThargoidKills
    also store complete reward for each class

--- KillCounter.cpp
+++ KillCounter.cpp
@@ -2,25 +2,52 @@
 
 void KillCounter::addKill(Thargoid type)
 {
+	addKill(type, type);
+}
+
+void KillCounter::addKill(Thargoid type, int reward)
+{
 	switch (type)
 	{
 	case Thargoid::Scout:
 		Scout_++;
+		Scout_reward += reward;
 		break;
 	case Thargoid::Cyclops:
 		Cyclops_++;
+		Cyclops_reward += reward;
 		break;
 	case Thargoid::Basilisk:
 		Basilisk_++;
+		Basilisk_reward += reward;
 		break;
 	case Thargoid::Medusa:
 		Medusa_++;
+		Medusa_reward += reward;
 		break;
 	case Thargoid::Hydra:
 		Hydra_++;
+		Hydra_reward += reward;
 		break;
 	default:
 		Unknown_++;
+		Unknown_reward += reward;
 		break;
 	}
+}
+
+void KillCounter::mergeList(std::list<ThargoidKill>& list)
+{
+	if (list.empty()) return;
+	auto sorter = [](ThargoidKill& first, ThargoidKill& second) {
+		return first.time_point() <= second.time_point();
+	};
+	if (kill_list.empty()) {
+		kill_list.splice(std::end(kill_list), list);
+		kill_list.sort(sorter);
+	}
+	else
+	{
+		kill_list.merge(list, sorter);
+	}	
 }
