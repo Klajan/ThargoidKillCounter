3e4b030e56dd686c6bf63bc5396afd591d6c0053
Klajan <48261494+Klajan@users.noreply.github.com>
2022-07-26 15:07:23 +0200

    Clean up main function

--- src/ThargoidKillCounter.cpp
+++ src/ThargoidKillCounter.cpp
@@ -11,10 +11,6 @@ int main()
 {
     OutputHandler output;
     std::wstring logfolder = getDefaultLogFolder();
-    LogDirReader reader = LogDirReader(logfolder);
-    LogReader parser = LogReader();
-    KillCounter kills = KillCounter();
-    EventProcessor proc = EventProcessor(kills);
     std::future<void> waitToDone;
     JournalProcessor processor;
 
@@ -22,20 +18,8 @@ int main()
     waitToDone = processor.start();
     waitToDone.wait();
     processor.stop();
-    /*
-    reader.readDir();
-    while (auto file = reader.getNextFile())
-    {
-        std::queue<std::unique_ptr<Journal::JournalEvent>> *qu = new std::queue<std::unique_ptr<Journal::JournalEvent>>();
-        std::cout << "Reading:" << file.value().filename() << "..." << std::endl;
-        //parser.readFile(file.value(), kills);
-        //parser.readFile(file.value(), *qu);
-        //proc.processEventeQueue(std::unique_ptr<std::queue<std::unique_ptr<Journal::JournalEvent>>>(qu));
 
-    }
-    */
-    std::cout << std::endl << std::endl;
-    kills = processor.getKillCounter();
+    KillCounter kills = processor.getKillCounter();
     output.killsToTable(kills);
 
     //wait for intput to close window
